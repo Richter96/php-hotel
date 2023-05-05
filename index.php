ab7e3e2aca3e2944eae5eb12721a7f1028f3b3fd
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-05 10:27:58 +0200

    add if for parking

--- index.php
+++ index.php
@@ -67,6 +67,11 @@ $voto = $_GET['voto'] ?? 'nobody';
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- fontawsone -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
 </head>
 
 
@@ -109,7 +114,18 @@ $voto = $_GET['voto'] ?? 'nobody';
                                 <?php if ($hotel['parking'] == $parking && $hotel['vote'] >= $voto) : ?>
                                     <tr class="">
                                         <?php foreach ($hotel as $key => $value) : ?>
-                                            <td scope="row"> <?= $value ?></td>
+                                            <!-- se la key = parking allora (if parking(value)=true 'si' else 'no') -->
+                                            <?php if ($key === 'parking') : ?>
+                                                <?php if ($hotel['parking'] == true) : ?>
+                                                    <td scope="row"><i class="fa-solid fa-car"></i></td>
+                                                <?php else : ?>
+                                                    <td scope="row"><i class="fa-solid fa-x"></i></td>
+                                                <?php endif ?>
+
+                                            <?php else : ?>
+                                                <td scope="row"> <?= $value ?></td>
+                                            <?php endif ?>
+
                                         <?php endforeach ?>
                                     </tr>
                                 <?php endif ?>
@@ -118,7 +134,16 @@ $voto = $_GET['voto'] ?? 'nobody';
                                 <?php if ($hotel['vote'] >= $voto) : ?>
                                     <tr class="">
                                         <?php foreach ($hotel as $key => $value) : ?>
-                                            <td scope="row"> <?= $value ?></td>
+                                            <?php if ($key === 'parking') : ?>
+                                                <?php if ($hotel['parking'] == true) : ?>
+                                                    <td scope="row"><i class="fa-solid fa-car"></i></td>
+                                                <?php else : ?>
+                                                    <td scope="row"><i class="fa-solid fa-x"></i></td>
+                                                <?php endif ?>
+
+                                            <?php else : ?>
+                                                <td scope="row"> <?= $value ?></td>
+                                            <?php endif ?>
                                         <?php endforeach ?>
                                     </tr>
                                 <?php endif ?>
